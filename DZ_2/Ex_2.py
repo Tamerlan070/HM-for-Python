bf285b7b247560db1b57510789f3ced329c19cdb
Tamerlan <tamerlan.teshev@icloud.com>
2022-10-01 17:51:19 +0300

    Add 2 ex

--- DZ_2/Ex_2.py
+++ DZ_2/Ex_2.py
@@ -1,10 +1,16 @@
 # 2. Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
-print ("Введите число N: ")
-N = int(input())
-factorial = list() # задали пустой список 
-for i in range (0, N):
-    N = N -1
-    factorial.append(N*(N-1))
-    
+def factorial(num):
+    result = 1
+    for i in range (1, num+1):
+        result = result*i
+    return result
 
-print (factorial)
+
+def sequence (number):
+    result_list = []
+    for i in range (1, number+1):
+        result_list.append(factorial(i))
+    return result_list
+
+N = int(input())
+print (sequence(N))
