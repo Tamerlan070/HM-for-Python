be1bf2422090e4ba3ab1d63ad49596db2db73893
Tamerlan <tamerlan.teshev@icloud.com>
2022-10-01 17:54:57 +0300

    Add 3 ex

--- DZ_2/Ex_3.py
+++ DZ_2/Ex_3.py
@@ -1,3 +1,10 @@
 # 3. Задайте список из n чисел, заполненный по формуле (1 + 1/n) ** n и выведите на экран их сумму.
-def summa_n (N: int )-> list:
-    
+print ('Введите число n: ')
+n = int(input())
+
+def sequence(n):
+
+    return[round((1 + 1 / x)**x, 2) for x in range (1, n + 1)]   
+        
+print(sequence(n))
+print(round(sum(sequence(n))))
